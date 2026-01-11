3390d9ebe0f627068e068866078b3f0fd0466e43
Paulo Abreu <paulosantana.thera@suzano.com.br>
2026-01-10 21:30:33 -0300

    refactor: improve GA4 configuration

--- components/ga-pageview.tsx
+++ components/ga-pageview.tsx
@@ -1,17 +1,16 @@
 "use client"
 
 import { pageview } from "@/lib/ga"
-import { usePathname, useSearchParams } from "next/navigation"
+import { usePathname } from "next/navigation"
 import { useEffect } from "react"
 
 export default function GAPageView() {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
 
   useEffect(() => {
-    const url = pathname + searchParams.toString()
-    pageview(url)
-  }, [pathname, searchParams])
+    if (!pathname) return
+    pageview(pathname)
+  }, [pathname])
 
   return null
 }
